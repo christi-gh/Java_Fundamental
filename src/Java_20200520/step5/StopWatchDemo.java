f3b2d91289ea0352eff198e36b4a1f26c136d50d
christi-gh <pjh6213@nate.com>
2020-05-20 18:00:22 +0900

    2020-05-20-v2

--- src/Java_20200520/step5/StopWatchDemo.java
+++ src/Java_20200520/step5/StopWatchDemo.java
@@ -1,17 +1,27 @@
 package Java_20200520.step5;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class StopWatchDemo  {
-	public static void run(StopMilliWatch s1) {
+	public static void run(StopWatch s1) {
 		s1.execute();
 	}
-	
-	public static void run(StopNanoWatch s1) {
-		s1.execute();
-	}
-	
-	public static void main(String[] args) {
+
+	public static void main(String[] args) throws Exception {
 		//StopNanoWatch s1 = new StopNanoWatch();
-		StopMilliWatch s1 = new StopMilliWatch();
-		run(s1);
+		//StopMilliWatch s1 = new StopMilliWatch();
+		//run(s1);
+		
+		BufferedReader br = new BufferedReader( new FileReader("C:\\dev\\eclipse-workspace\\Java_Fundamental\\src\\Java_20200520\\step5\\class.txt"));
+		
+		String readLine = br.readLine();
+		
+		
+		Class clazz = Class.forName(readLine);
+		
+		StopWatch s = (StopWatch)clazz.newInstance();
+		run(s);
 	}
 }
